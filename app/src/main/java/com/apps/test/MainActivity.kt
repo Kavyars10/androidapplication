194c020603e99d11a971a9dbb2a6b9cf23aefd46
kavya Rajarathinam Srinivasan <kavya.jj@gmail.com>
2023-02-06 14:52:48 +0100

    Initial commit

---
+++ app/src/main/java/com/apps/test/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.apps.test
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.usercentrics.sdk.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val showFirstLayerButton by lazy { findViewById<View>(R.id.show_first_layer) }
+    private val showSecondLayerButton by lazy { findViewById<View>(R.id.show_second_layer) }
+    private var banner: UsercentricsBanner? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 'isReady' is called after Usercentrics has finished initializing
+        // get the consent status of the user, via UsercentricsReadyStatus
+        Usercentrics.isReady(onSuccess = { status ->
+            if (status.shouldCollectConsent) {
+                showFirstLayer()
+            } else {
+                applyConsent(status.consents)
+            }
+
+            bindContent()
+        }, onFailure = {
+            // Handle error
+            it.printStackTrace()
+        })
+    }
+
+    override fun onDestroy() {
+        banner?.dismiss()
+        banner = null
+        super.onDestroy()
+    }
+
+    private fun applyConsent(consents: List<UsercentricsServiceConsent>) {
+        // https://docs.usercentrics.com/cmp_in_app_sdk/latest/apply_consent/apply-consent/#apply-consent-to-each-service
+    }
+
+    private fun showFirstLayer(
+        layout: UsercentricsLayout = UsercentricsLayout.Popup(PopupPosition.BOTTOM),
+        settings: BannerSettings? = null
+    ) {
+        // Launch Usercentrics Banner with your settings
+        banner = UsercentricsBanner(this, settings).also {
+            it.showFirstLayer(
+                layout = layout,
+                callback = ::handleUserResponse
+            )
+        }
+    }
+
+    private fun showSecondLayer() {
+        // This is useful when you need to call our CMP from settings screen for instance, therefore the user may dismiss the view
+        val settings = BannerSettings(
+            secondLayerStyleSettings = SecondLayerStyleSettings(
+                showCloseButton = true,
+            )
+        )
+        banner = UsercentricsBanner(this, settings).also {
+            it.showSecondLayer(
+                callback = ::handleUserResponse
+            )
+        }
+    }
+
+    private fun handleUserResponse(userResponse: UsercentricsConsentUserResponse?) {
+        userResponse ?: return
+
+        println("Consents -> ${userResponse.consents}")
+        println("User Interaction -> ${userResponse.userInteraction}")
+        println("Controller Id -> ${userResponse.controllerId}")
+
+        applyConsent(userResponse.consents)
+    }
+
+    private fun bindContent() {
+        showFirstLayerButton.setOnClickListener {
+            showFirstLayer()
+        }
+        showSecondLayerButton.setOnClickListener {
+            showSecondLayer()
+        }
+
+        showFirstLayerButton.isEnabled = true
+        showSecondLayerButton.isEnabled = true
+    }
+}
---
+++ app/src/main/java/com/apps/test/MainApplication.kt
@@ -0,0 +1,22 @@
+package com.apps.test
+
+import android.app.Application
+import com.usercentrics.sdk.Usercentrics
+import com.usercentrics.sdk.UsercentricsOptions
+import com.usercentrics.sdk.models.common.UsercentricsLoggerLevel
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialize Usercentrics with your configuration
+        val options = UsercentricsOptions(
+            settingsId = "ICiEPJ4hgY-dQc",
+            loggerLevel = UsercentricsLoggerLevel.DEBUG,
+        )
+
+        Usercentrics.initialize(this, options)
+    }
+
+}
